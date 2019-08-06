5f4cbdce2ce751da029712276d11cc9a3323ea73
ChiaYing Ho <joecbgb@gmail.com>
2019-08-06 17:30:10 +0800

    complete about and contact

--- app.js
+++ app.js
@@ -15,12 +15,27 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function(req, res) {
+    res.render("home", {
+        StartingContent:
+        homeStartingContent
+    });
+});
 
 
+app.get("/about", function(req, res) {
+    res.render("about", {
+        AboutContent:
+        aboutContent
+    });
+});
 
-
-
-
+app.get("/contact",function (req, res) {
+    res.render("contact", {
+        ContactContent:
+        contactContent
+    });
+});
 
 
 
