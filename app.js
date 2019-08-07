4602d71ea37e984ef66635730e460a665d51a4a3
ChiaYing Ho <joecbgb@gmail.com>
2019-08-07 16:57:19 +0800

    render posts done

--- app.js
+++ app.js
@@ -15,10 +15,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
+
 app.get("/", function(req, res) {
     res.render("home", {
         StartingContent:
-        homeStartingContent
+        homeStartingContent,
+        posts:
+        posts
     });
 });
 
@@ -37,8 +42,18 @@ app.get("/contact",function (req, res) {
     });
 });
 
+app.get("/compose", function(req, res) {
+    res.render("compose")
+});
 
-
+app.post("/compose", function(req, res){
+    let post = {
+        title: req.body.postTitle,
+        content: req.body.postBody
+    };
+    posts.push(post);
+    res.redirect("/");
+});
 
 
 
